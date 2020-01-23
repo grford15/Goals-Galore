fcd20257489efe0cdb2b0fe39e32184e6af6c82e
Greg <g.rutherford10@gmail.com>
2020-01-23 10:24:16 +0000

    add navbar to app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="container-fluid">
-      <div className="row justify-content-center">
-        <h1>Goals Galore</h1>
+    <>
+      <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+          Goals Galore
+        </a>
+      </nav>
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <h1>Goals Galore</h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
