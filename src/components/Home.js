1766619932398d3f4200c62de05d01505fff2534
Greg <g.rutherford10@gmail.com>
2020-01-25 14:37:33 +0000

    reworked to include iframe on single game

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,28 +21,38 @@ export default class Home extends Component {
 
   render() {
     let { loaded, games } = this.state;
+    let game = games[0];
 
     return (
       <div>
         {loaded ? (
-          games.map((game, index) => (
-            <div key={index} className="row align-items-center my-4">
-              <div className="col">
-                <h4>{game.title}</h4>
-                <p>{game.competition.name}</p>
-                <a href={game.url} className="btn btn-primary">
-                  Watch the highlights here
-                </a>
-              </div>
-              <div className="col">
-                <img
-                  src={game.thumbnail}
-                  alt="game thumbnail"
-                  style={{ width: '150px', height: '150px' }}
-                />
-              </div>
-            </div>
-          ))
+          //   games.map((game, index) => (
+          //     <div key={index} className="row align-items-center my-4">
+          //       <div className="col">
+          //         <h4>{game.title}</h4>
+          //         <p>{game.competition.name}</p>
+          //         <a href={game.url} className="btn btn-primary">
+          //           Watch the highlights here
+          //         </a>
+          //       </div>
+          //       <div className="col">
+          //         <img
+          //           src={game.thumbnail}
+          //           alt="game thumbnail"
+          //           style={{ width: '150px', height: '150px' }}
+          //         />
+          //       </div>
+          //     </div>
+          //   ))
+          <div>
+            <h4>{game.title}</h4>
+            <iframe
+              title={game.title}
+              src="https://www.scorebat.com/embed/g/878324/?s=2"
+              width="560"
+              height="650"
+            ></iframe>
+          </div>
         ) : (
           <p>Loading ... </p>
         )}
